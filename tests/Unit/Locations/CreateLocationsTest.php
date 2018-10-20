951917af4968c7d57486dcb8276a9ef914917767
Rafael Comonfort <a.comonfort@outlook.com>
2018-10-20 11:44:57 -0500

    Finished ticket testing

---
+++ tests/Unit/Locations/CreateLocationsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit\Locations;
+
+use Tests\TestCase;
+use Tests\Helper;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CreateLocationTest extends TestCase
+{
+
+}
