65abf30dbc4821a994409b77aace997d9483bb4f
Carlos2396 <carlosaam96@gmail.com>
2018-11-17 12:26:34 -0600

    Created PasswordReset model and two notifications.

---
+++ app/Notifications/PasswordResetRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class PasswordResetRequest extends Notification
+{
+    use Queueable;
+
+    protected $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = env('FRONTEND_ENDPOINT', '127.0.0.1:4200').'/reestablecer/'.$this->token;
+
+        return (new MailMessage)
+                    ->line('Te enviamos este correo porque solicitaste reiniciar la contraseña de tu cuenta en Events.')
+                    ->action('Reestablecer contraseña', url($url))
+                    ->line('Si tu no solicitaste esta acción, no te preocupes, tu cuenta está segura.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
